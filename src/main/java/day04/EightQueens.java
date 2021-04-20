f73e4a731f890f6f0a5a3862b1a4b57c474fc00c
stailer@126.com <stailer@126.com>
2021-04-20 13:02:46 +0800

    Add execrise 4

---
+++ src/main/java/day04/EightQueens.java
@@ -0,0 +1,66 @@
+package day04;
+
+import tools.AlgorithmTools.*;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Predicate;
+
+public class EightQueen {
+
+    public static Set<Set<Pair<Integer, Integer>>> solve(Integer dimension, Integer Queens){
+        Set<Pair<Integer, Integer>> available = new HashSet<>();
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                available.add(new Pair<>(i, j));
+            }
+        }
+
+        Set<Set<Pair<Integer, Integer>>> solved = new HashSet<>();
+        backpack(available, new HashSet<>(), Queens, solved);
+        return solved;
+    }
+
+    private static void backpack(Set<Pair<Integer, Integer>> available,
+                                 Set<Pair<Integer, Integer>> installed,
+                                 Integer Queens,
+                                 Set<Set<Pair<Integer, Integer>>> solved){
+        if(installed.size() == Queens){
+            solved.add(installed);
+        }
+
+        for(Pair<Integer, Integer> index : available){
+            Set<Pair<Integer, Integer>> newInstalled = new HashSet<>();
+            Set<Pair<Integer, Integer>> newAvailable = new HashSet<>();
+
+            newInstalled.addAll(installed);
+            newInstalled.add(index);
+
+            newAvailable.addAll(available);
+            newAvailable.removeIf(new Predicate<Pair<Integer, Integer>>() {
+                @Override
+                public boolean test(Pair<Integer, Integer> pair) {
+
+                    return pair.i == index.i ||
+                            pair.j == index.j ||
+                            (pair.i - index.i) == (pair.j - index.j);
+                }
+            });
+
+            backpack(newAvailable, newInstalled, Queens, solved);
+        }
+
+
+    }
+
+    private static class ComparablePair extends Pair<Integer, Integer>
+                                        implements Comparable<ComparablePair>{
+        @Override
+        public int compareTo(ComparablePair o) {
+            if(this.i != o.i)return this.i - o.i;
+            else return this.j - o.j;
+        }
+
+
+    }
+}
